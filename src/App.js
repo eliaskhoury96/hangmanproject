2c5d43ed1de735eb5e071c567a9eff4547a97ad1
Your username <ek29091@gmail.com>
2024-01-30 19:27:50 +0200

    add

--- src/App.js
+++ src/App.js
@@ -13,33 +13,48 @@ function App() {
         }
         return letterStatus
     }
+   const[gameOver, setGameOver] = useState(false)
 
+   const[isGameWon, setIsGameWon] = useState(false)
     const [letterStatus, setLetterStatus] = useState(generateLetterStatuses());
     const [solution, setSolution] = useState({
         word: 'CODE',
         hint: 'Your ideal mood when coding'
     });
     const [score, setScore] = useState(100);
-
+    
     const handleLetterClick = (letter) => {
         if (!letterStatus[letter]) {
             const isLetterInWord = solution.word.includes(letter);
             const newScore = score + (isLetterInWord ? 5 : -20);
             const newLetterStatus = { ...letterStatus, [letter]: true };
+            const wordGuessed = solution.word.split('').every((letter) => newLetterStatus[letter]);  
+            if (wordGuessed) {
+             setIsGameWon(true) 
+             setGameOver(true)
+          }
+          if (newScore <= 0){
+            setIsGameWon(false) 
+            setGameOver(true)
 
+          }
             setLetterStatus(newLetterStatus);
             setScore(newScore);
         }
     };
+  
 
     return (
         <div>
+           {!gameOver ? <div>
             <Score score={score} />
             <Solution letterStatus={letterStatus} solution={solution} />
-            <Letters letterStatus={letterStatus} onLetterClick={handleLetterClick} />
-            <EndGame letterStatus={letterStatus} score={score} solution={solution} />
+            <Letters letterStatus={letterStatus} onLetterClick={handleLetterClick} /></div>
+            :
+          
+            <EndGame letterStatus={letterStatus} score={score} solution={solution} isGameWon={isGameWon} />}
         </div>
     );
-}
+           }
 
 export default App;
--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,20 +1,9 @@
 import React from 'react';
-function EndGame({ letterStatus, score, solution }) {
-    const checkGameOver = () => {
-        const wordGuessed = solution.word.split('').every((letter) => letterStatus[letter]);
-        if (wordGuessed) {
-            return { isGameWon: true, gameOver: true };
-        }
-        else if (score <= 0) {
-            return { isGameWon: false, gameOver: true };
-        }
-        return { isGameWon: false, gameOver: false };
-    };
-    const { isGameWon, gameOver } = checkGameOver();
-
+function EndGame({solution, isGameWon }) {
+ 
     return (
         <div>
-            {gameOver ? (
+           
                 <div>
                     {isGameWon ? (
                         <p>Congratulations! You guessed the word: {solution.word}</p>
@@ -22,7 +11,6 @@ function EndGame({ letterStatus, score, solution }) {
                         <p>Game over! The word was: {solution.word}</p>
                     )}
                 </div>
-            ) : null}
         </div>
     );
 }
